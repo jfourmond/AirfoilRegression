cf76886f2d0d022692045c40df0cdb4f5909ef26
jfourmond <jerome@fourmond.fr>
2017-03-16 12:00:06 +0100

    Updated AirfoilRegression

--- src/main/java/AirfoilRegression.java
+++ src/main/java/AirfoilRegression.java
@@ -3,7 +3,6 @@ import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
 import org.datavec.api.split.FileSplit;
 import org.deeplearning4j.api.storage.StatsStorage;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
-import org.deeplearning4j.eval.Evaluation;
 import org.deeplearning4j.eval.RegressionEvaluation;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
@@ -48,7 +47,7 @@ public class AirfoilRegression {
     private static final int numHidden = 5;
     private static final int numOutputs = 1;
     private static final double learningRate = 0.1;
-    private static final int iterations = 1000;
+    private static final int iterations = 10000;
     private static final long seed = 13;
 
     private static MultiLayerNetwork net;
@@ -65,7 +64,6 @@ public class AirfoilRegression {
         RecordReader recordReader = new CSVRecordReader(numLinesToSkip, delimiter);
         recordReader.initialize(new FileSplit(new ClassPathResource(FILENAME).getFile()));
         DataSetIterator iterator = new RecordReaderDataSetIterator(recordReader, batchSize, 5, 5, true);
-//        DataSetIterator iterator = new RecordReaderDataSetIterator(recordReader, batchSize, labelIndex, numClasses, true);
         allData = iterator.next();
         allData.shuffle();
 
@@ -89,7 +87,7 @@ public class AirfoilRegression {
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
                 .seed(seed)
                 .iterations(iterations)
-                .activation(Activation.SIGMOID)
+                .activation(Activation.TANH)
                 .weightInit(WeightInit.XAVIER)
                 .learningRate(learningRate)
                 .regularization(true).l2(1e-4)
@@ -97,7 +95,6 @@ public class AirfoilRegression {
                 .layer(0, new DenseLayer.Builder().nIn(numInputs).nOut(numHidden)
                         .build())
                 .layer(1, new DenseLayer.Builder().nIn(numHidden).nOut(numHidden)
-                        .activation(Activation.RELU)
                         .build())
                 .layer(2, new OutputLayer.Builder(LossFunctions.LossFunction.MSE)
                         .activation(Activation.IDENTITY)
@@ -108,7 +105,7 @@ public class AirfoilRegression {
         Log.info("Building neural network...");
         net = new MultiLayerNetwork(conf);
         net.init();
-        net.setListeners(new ScoreIterationListener(100), new StatsListener(statsStorage));
+        net.setListeners(new ScoreIterationListener(1), new StatsListener(statsStorage));
     }
 
     private static void train() {
@@ -123,6 +120,7 @@ public class AirfoilRegression {
         RegressionEvaluation re = new RegressionEvaluation(5);
         INDArray output = net.output(testData.getFeatureMatrix());
         re.eval(testData.getLabels(), output);
+
         Log.info(re.stats());
 
 //        Evaluation eval = new Evaluation(0);
@@ -143,5 +141,4 @@ public class AirfoilRegression {
         train();
         evaluate();
     }
-
 }
